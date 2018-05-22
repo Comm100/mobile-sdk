e4628f58d488820f1abfc4dcd253fa987d47717a
Allon <allon.lu@comm100.com>
2018-05-22 15:06:32 +0800

    comment auto submit prechat because custom js issue

--- comm100-livechat-ios-sample/WebViewChatWindow/HomeViewController.m
+++ comm100-livechat-ios-sample/WebViewChatWindow/HomeViewController.m
@@ -20,7 +20,7 @@
 -(IBAction)startChat:(id)sender{
     
     // please replace your own siteId & planId
-    NSString * chatUrl = @"https://chatserver.comm100.com/chatwindowmobile.aspx?siteId=10000&planId=5000239";
+    NSString * chatUrl = @"https://chatserver.comm100.com/chatwindow.aspx?siteId=10000&planId=5000239";
     
     VisitorClientController * visitorClient = [[VisitorClientController alloc] initWithChatUrl:chatUrl];
     
@@ -35,16 +35,16 @@
      *  "department": Department field
      *  "ticket": Ticket field
      */
-    Field * name = [[Field alloc] initWithType:@"name" value:@"name-allon2233"];
-    Field * email = [[Field alloc] initWithType:@"email" value:@"email-allon2233@email.com"];
+    //Field * name = [[Field alloc] initWithType:@"name" value:@"name-allon2233"];
+    //Field * email = [[Field alloc] initWithType:@"email" value:@"email-allon2233@email.com"];
     
     /* ## Custom Fields ##
      *  For custom fields you should use field label to identify. This is the field name you defined in prechat.
      */
-    Field * account = [[Field alloc] initWithLabel:@"Account" value:@"allon account"];
+    //Field * account = [[Field alloc] initWithLabel:@"Account" value:@"allon account"];
     
     // fill prechat form and skip (auto submit) these in chat window.
-    [visitorClient fillingPrechat:@[name, email, account] isSkip:YES];
+    //[visitorClient fillingPrechat:@[name, email, account] isSkip:YES];
     
     UINavModalWebView * navController = [[UINavModalWebView alloc] initWithRootViewController:visitorClient];
     
